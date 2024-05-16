0c91a52ffa6d3e0ab7148f1614a84d279c819e1a
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-05-15 22:52:58 -0300

    k

---
+++ aula_basica/exercicio.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Números a serem divididos
+  int numerador = 10;
+  int denominador = 2;
+
+  // Realizando a divisão sem usar o operador de divisão
+  int resultado = numerador * (1 / denominador).toInt();
+
+  // Imprimindo o resultado
+  print("Resultado da divisão: $resultado");
+}
